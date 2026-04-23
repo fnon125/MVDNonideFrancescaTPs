4cd0b482e51473ef4645e29255ffba82b56d9c25
fnon125 <fnonide@udesa.edu.ar>
2026-04-23 18:48:03 -0300

    Tabla scraping_oea con titulos y cuerpos

--- TP2/scripts/scraping_oea.R
+++ TP2/scripts/scraping_oea.R
@@ -63,3 +63,72 @@ html_oea(url = url_oas[2], mes = 2, ruta = data_dir)
 html_oea(url = url_oas[3], mes = 3, ruta = data_dir)
 html_oea(url = url_oas[4], mes = 4, ruta = data_dir)
 
+############# tabla id, titulo, cuerpo ############# 
+# Hacemos una funcion que busque el html que descargamos anteriormente y extrae
+# los titulos y links de cada comunicado usando el selector ".itemmenulink" que obtuvimos con SelectorGadget. 
+# Llamamos a la funcion para cada uno de los cuatro meses y unimos los resultados en una tabla con bind_rows.
+# Nos devuelve un tibble con tres columnas (1) el mes del comunicado, (2) el titulo del comunicado
+# y (3) el nro de fila como id del comunicado 
+scrapear_comunicado <- function(mes,ruta) {
+  archivos <- list.files(ruta, pattern = paste0("oea_mes", mes, "_2026"), full.names = TRUE) #busco los links que ya descargue en data
+  pagina_html <- read_html(archivos[1])
+  
+  css_titulos <- pagina_html %>%
+    html_elements(".itemmenulink")
+  
+  titulos <- css_titulos %>%
+    html_text2() %>% #extraemos tecto limpio de cada nodo
+    str_trim() 
+    
+  links <- css_titulos %>%
+    html_attr("href")  %>% #Accedemos al atributo href para obtener las urls de cada noticia
+    paste0("https://www.oas.org/es/centro_noticias/", .) 
+  
+  cat(length(titulos), "titulos extraidos en el mes", mes, "\n")
+  
+  tibble(
+    mes = mes,
+    titulo = titulos,
+    link = links)}
+
+# llamo a la funcion de 01 a 04 y los uno a una sola tabla con bind_rows
+tabla_links <- bind_rows(
+  scrapear_comunicado(mes= 1, ruta= data_dir),
+  scrapear_comunicado(mes= 2, ruta= data_dir),
+  scrapear_comunicado(mes= 3, ruta= data_dir),
+  scrapear_comunicado(mes= 4, ruta= data_dir)) %>%
+  mutate(id = row_number())
+
+# Hacemos una funcion para extraer el cuerpo de los comunicados. La funcion scrapear_cuerpo extrae los parrafos de cuerpo de 
+# cada noticia usando el selector "#rightmaincol p" que extraimos con SelectorGadget. Le pedimos que para cada request se tome un 
+# break de 3 segundos como ya hicimos anteriormente. Para llamar a la funcion, la aplicamos a cada link de tabla_links
+# para obtener la tabla final con (1) id, (2) titulo, (3) cuerpo. La guardamos en un rds en data. 
+scrapear_cuerpo <- function(url) {
+  Sys.sleep(3)
+  html_comunicado <- read_html(url)
+  
+  cuerpo <- html_comunicado %>%
+    html_elements("#rightmaincol p") %>%
+    html_text2() %>%
+    str_trim()
+  
+  cuerpo <- str_c(cuerpo, collapse = " ") #concatenamos todos los parrafos. Similar a lo que hicimos en la tutorial 5. 
+  return(cuerpo)}
+
+# aplicamos la funcion a cada link con map_chr() como en la tut. 
+cuerpos_oea <- tabla_links %>%
+  select(id, link) %>%
+  mutate(cuerpo = map_chr(link, scrapear_cuerpo))
+
+# uno titulos y cuerpo por id 
+tabla_comunicados <- tabla_links %>%
+  left_join(cuerpos_oea, by = "id") %>%
+  select(id, titulo, cuerpo) %>%
+  write_rds(here(data_dir, "comunicados_oea.rds"))
+
+message("Tabla guardada en: ", here(data_dir, "comunicados_oea.rds"))
+
+
+
+
+
